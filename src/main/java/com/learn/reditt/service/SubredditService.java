febbcb80678e2d75784188eba21aac66e5fcc2ea
Tribhuwan Dewangan <dewangan.tribhuwan@gmail.com>
2021-08-06 19:45:53 +0530

    get secured content

---
+++ src/main/java/com/learn/reditt/service/SubredditService.java
@@ -0,0 +1,44 @@
+package com.learn.reditt.service;
+
+import com.learn.reditt.dto.SubredditDto;
+import com.learn.reditt.model.Subreddit;
+import com.learn.reditt.repository.SubredditRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class SubredditService {
+    private  final SubredditRepository subredditRepository;
+    public SubredditDto save(SubredditDto subredditDto) {
+        Subreddit save = subredditRepository.save(mapSubredditDto(subredditDto));
+        subredditDto.setId(save.getId());
+        return subredditDto;
+    }
+
+    @Transactional
+    private Subreddit mapSubredditDto(SubredditDto subredditDto) {
+        return Subreddit.builder().name(subredditDto.getName())
+                .description(subredditDto.getDescription()).build();
+    }
+
+    @Transactional(readOnly = true)
+    public List<SubredditDto> getAll() {
+        return subredditRepository.findAll().stream().map((this::mapToDto))
+                .collect(toList());
+    }
+
+    private SubredditDto mapToDto(Subreddit subreddit) {
+        return SubredditDto.builder().name(subreddit.getName())
+                .id(subreddit.getId())
+                .numberOfPosts(subreddit.getPosts().size())
+                .build();
+    }
+}
